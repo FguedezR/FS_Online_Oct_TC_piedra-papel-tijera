0cf7579ecb88133a0f3efeab616f431b8a0f3e5b
Fernando <fernanguedezr@gmail.com>
2025-11-08 14:06:22 +0100

    js terminado

--- js/script.js
+++ js/script.js
@@ -5,15 +5,26 @@ const contadorUsuario = document.getElementById("contador-usuario");
 const contadorOrdenador = document.getElementById("contador-ordenador");
 const botonesJugada = document.querySelectorAll(".boton-jugada");
 
+function obtenerJugadaOrdenador() {
+    const jugadasPosibles = ["piedra", "papel", "tijera"];
+    const indiceAleatorio = Math.floor(Math.random() * jugadasPosibles.length);
+    return jugadasPosibles[indiceAleatorio];
+}
+
 let puntosUsuario = 0;
 let puntosOrdenador = 0;
 
 botonesJugada.forEach((boton) => {
   boton.addEventListener("click", () => {
     const eleccionUsuario = boton.dataset.jugada;
+    const eleccionOrdenador = obtenerJugadaOrdenador();
+    const resultado = obtenerResultado(eleccionOrdenador, eleccionUsuario);
+    mostrarResultado(resultado,eleccionUsuario,eleccionOrdenador);
+    actualizarPuntuacion(resultado);
     console.log("Elección", eleccionUsuario);
   });
 });
+
 
 function obtenerResultado(usuario, ordenador) {
   if (usuario === ordenador) {
@@ -29,16 +40,32 @@ function obtenerResultado(usuario, ordenador) {
   }
 }
 
-function mostrarResultado(a, b, c) {
 
+function mostrarResultado(resultado, eleccionUsuario, eleccionOrdenador) {
+    let mensaje;
+    if (resultado === 'Ganaste') {
+        mensaje = `Has ganado esta ronda: ${eleccionUsuario}. Máquina: ${eleccionOrdenador}.`
+    } else if (resultado === 'Perdiste') {
+        mensaje = `Has perdido esta ronda: ${eleccionUsuario}. Máquina: ${eleccionOrdenador}.`
+    } else {
+        mensaje = `Empate ambos eligieron ${eleccionUsuario}.`
+    }
+    contadorResultados.innerHTML = mensaje;
 }
+
+
+const jugadaPC = contadorResultados;
+console.log('la jugada es', jugadaPC);
 
 
 // resultado
-function actualizarPuntuacion(a) {
-    if (a === 'Ganaste') {
+function actualizarPuntuacion(resultado) {
+    if (resultado === 'Ganaste') {
         puntosUsuario++;
-    } else if (a === 'Perdiste') {
+    } else if (resultado === 'Perdiste') {
         puntosOrdenador++;
     }
+    contadorUsuario.innerHTML = puntosUsuario;
+    contadorOrdenador.innerHTML = puntosOrdenador;
 }
+
